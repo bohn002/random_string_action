8377a6f30a8012af57b7f47897d14f1a434c8202
Nate Bohn <nathanbohn@gmail.com>
2021-09-19 22:13:06 -0500

    switching case

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ import string
 import random
 
 def main():
+    letters = ""
     char_type = os.environ['INPUT_CHAR_TYPE']
-    char_length = os.environ['INPUT_CHAR_LENGTH']
-    match char_type:
-        case "lower":
-            letters = string.ascii_lowercase
-        case "upper":
-            letters = string.ascii_uppercase
-        case _:
-            letters = string.ascii_letters
+    char_length = int(os.environ['INPUT_CHAR_LENGTH'])
+    if char_type == "lower":
+        letters = string.ascii_lowercase
+    elif char_type == "upper":
+        letters = string.ascii_uppercase
+    else:
+        letters = string.ascii_letters
     r_string = ''.join(random.choice(letters) for i in range(char_length))
     set_output(r_string)
 
