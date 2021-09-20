923f60957f14047217ffc53af68b09ff36a33537
Nate Bohn <nathanbohn@gmail.com>
2021-09-19 21:59:18 -0500

    trying output

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import os
 
 def main():
-    print(os.environ)
-    return "56"
+    char_type = os.environ['INPUT_CHAR_TYPE']
+    char_length = os.environ['INPUT_CHAR_LENGTH']
+    set_output("boop")
+
+def set_output(cmd):
+    print(f"::set-output name=random_string::{cmd}", flush=True)
 
 if __name__ == "__main__":
     main()
