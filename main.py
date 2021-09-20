ba5a500025c11e80e26308837e45b04e11911806
Nate Bohn <nathanbohn@gmail.com>
2021-09-19 22:09:28 -0500

    doing the random

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import os
+import string
+import random
 
 def main():
     char_type = os.environ['INPUT_CHAR_TYPE']
     char_length = os.environ['INPUT_CHAR_LENGTH']
-    set_output("boop")
+    match char_type:
+        case "lower":
+            letters = string.ascii_lowercase
+        case "upper":
+            letters = string.ascii_uppercase
+        case _:
+            letters = string.ascii_letters
+    r_string = ''.join(random.choice(letters) for i in range(char_length))
+    set_output(r_string)
 
 def set_output(cmd):
     print(f"::set-output name=random_string::{cmd}", flush=True)
